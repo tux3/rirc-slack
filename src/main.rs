6c7e20f0d5edfe68e2af10d14f4acc6f5a8871ce
tux3 <barrdetwix@gmail.com>
2018-03-10 03:27:48 +0100

    Start an example rIRC server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate rirc_server;
+
+use rirc_server::Server;
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "0.0.0.0:6697".parse().unwrap();
+    let mut server = Server::new(addr);
+
+    println!("rIRC Slack gateway");
+
+    server.start();
 }
