a59195ef31721266d0605374eaf9b213247240af
tux3 <barrdetwix@gmail.com>
2018-03-11 16:59:02 +0100

    Use defaults for ServerSettings not manually specified

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let mut server = Server::new(ServerSettings {
         listen_addr: "0.0.0.0:6697".parse().unwrap(),
         server_name: "rIRC-slack-gateway".to_owned(),
+        ..Default::default()
     });
 
     println!("rIRC Slack gateway");
